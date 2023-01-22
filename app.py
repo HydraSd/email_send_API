7754a3a6cb7694c465b29bd1f736129dc601e06d
Sandhil Doolwala <doolwala.sandhil@gmail.com>
2023-01-22 13:49:34 +0530

    cross_origin added

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request
 from send_mail import *
 app = Flask(__name__)
-# cors = CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/<name>/<email>/<contact>/<dis>', methods=['GET','POST'])
-# @cross_origin()
+@cross_origin()
 def sendMail(name, email, contact, dis):
     SendMessage(name, email, contact, dis)
     return jsonify({"name":name, "email": email, "contact":contact, "description":dis})
